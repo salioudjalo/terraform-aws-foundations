1d2d5c9851e33df077583cf0e20ca880929c784e
Saliou Djalo <djalosaliou@gmail.com>
2025-12-18 17:32:22 +0400

    Day 08: Formatting and comments

--- day08-vpc/main.tf
+++ day08-vpc/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_cidr = "10.0.0.0/16"
-  environment = "dev"
+  source       = "./modules/vpc"
+  vpc_cidr     = "10.0.0.0/16"
+  environment  = "dev"
   project_name = "day08-full-vpc"
-  list_az = ["us-east-1a","us-east-1b"]
+  list_az      = ["us-east-1a", "us-east-1b"] // do not mix Regions (us-east-1), and AZ (us-east-1a)
 }
 
-// no ressource in root module for the moment
+// no resource in root module for the moment
