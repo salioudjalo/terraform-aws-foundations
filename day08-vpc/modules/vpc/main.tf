3b72325ee64f0d6754f86704380ace105a7c2afa
Saliou Djalo <djalosaliou@gmail.com>
2025-12-19 11:56:39 +0400

    Day 8 Phase 5: NAT Gateway and private subnet routing

--- day08-vpc/modules/vpc/main.tf
+++ day08-vpc/modules/vpc/main.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "main_vpc" {
 }
 
 # Provides an VPC public subnet resource.
+# Only public subnets are associated with the IGW route table
 resource "aws_subnet" "public_subnets" {
   count                   = local.nb_of_public_subnets
   vpc_id                  = aws_vpc.main_vpc.id
@@ -39,4 +40,102 @@ resource "aws_subnet" "private_subnets" {
     Subnet = "private"
   }
 }
+
+/*
+A subnet is public only if:
+it has a route 0.0.0.0/0 → Internet Gateway
+AND instances can receive public IPs
+You already did the second part (map_public_ip_on_launch = true).
+*/
+
+# Provides a resource to create a VPC Internet Gateway.
+# An Internet Gateway is a VPC-level construct.
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Project = var.project_name
+    Env     = var.environment
+  }
+}
+
+# Provides a resource to create a VPC routing table.
+
+# Public Route Table
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  # 0.0.0.0/0 → IGW
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Project = var.project_name
+    Env     = var.environment
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "rt_private" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  # 0.0.0.0/0 → NAT Gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Project = var.project_name
+    Env     = var.environment
+  }
+}
+
+// Provides a resource to create an association between a route table and a subnet 
+// or a route table and an internet gateway or virtual private gateway.
+
+resource "aws_route_table_association" "rta_public" {
+  count          = local.nb_of_public_subnets
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.rt_public.id
+  // If a subnet is associated with a route to an Internet Gateway, it is PUBLIC.
+}
+
+resource "aws_route_table_association" "rta_private" {
+  count          = local.nb_of_private_subnets
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.rt_private.id
+  // Private subnets go to NAT route table
+}
+
+# Provides an Elastic IP resource.
+resource "aws_eip" "eip" {
+  domain = "vpc" // It must be VPC-scoped
+  tags = {
+    Project = var.project_name
+    Env     = var.environment
+  }
+}
+
+# Provides a resource to create a VPC NAT Gateway.
+# NAT Gateway costs money per hour.
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name    = "Public NAT-${var.project_name}-${var.project_name}"
+    Project = var.project_name
+    Env     = var.environment
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
+
 
