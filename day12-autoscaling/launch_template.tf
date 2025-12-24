8b3dd3b0d123aab3a604a23ee2cd13a15e8d3077
saliouveolia <saliou.djalo@veolia.com>
2025-12-24 15:49:01 +0400

    Day 12: base64encode for user_data.sh + new key pair for ssh

--- day12-autoscaling/launch_template.tf
+++ day12-autoscaling/launch_template.tf
@@ -5,9 +5,15 @@ resource "aws_launch_template" "ec2_template" {
   name                   = "day12-lt"
   image_id               = data.aws_ami.amazon_linux.image_id
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [data.terraform_remote_state.compute.outputs.app_ec2_id] # Your private EC2 SG (NOT bastion, NOT ALB)
-  key_name               = aws_key_pair.deployer.key_name
-  user_data              = file("./user_data.sh")
+  vpc_security_group_ids = [data.terraform_remote_state.compute.outputs.app_ec2_sg_id] # Your private EC2 SG (NOT bastion, NOT ALB)
+  key_name               = var.lt_key_name                                             # aws_key_pair.deployer.key_name
+
+
+  // aws_instance → accepts plain text user_data
+  // aws_launch_template → REQUIRES Base64-encoded user data
+  user_data = base64encode(
+    file("${path.module}/user_data.sh")
+  )
   tags = {
     Name    = "day12-lt"
     Env     = "dev"
@@ -16,8 +22,10 @@ resource "aws_launch_template" "ec2_template" {
 
 }
 
-# I need to generate a key_pair
+# if you need to generate a key_pair
+/*
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = file(var.ssh_public_key_path)
 }
+*/
--- day12-autoscaling/variables.tf
+++ day12-autoscaling/variables.tf
@@ -1,4 +1,11 @@
 variable "ssh_public_key_path" {
   description = "Path to SSH public key"
   type        = string
+  default     = "~/.ssh/deployer-key"
+}
+
+variable "lt_key_name" {
+  description = "Key pair name for SSH (generated from the AWS console)"
+  type        = string
+  default     = "launch-template-key"
 }
